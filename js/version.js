7dc79323654653ea5e03cd5e2da17e5b066878d0
figrita <josephdavidruiz@gmail.com>
2025-12-29 00:17:34 -0500

    remove beta designation from version.js

--- js/version.js
+++ js/version.js
@@ -12,7 +12,7 @@
  * - 0.1: Initial patch format
  * - 0.2: Current version (updated from 0.1)
  */
-export const PATCH_VERSION = '0.2.3-beta'
+export const PATCH_VERSION = '0.2.3'
 
 /**
  * Get the current patch format version
