299b36946f7ce3a0c255ff367f958fc924abb0c9
figrita <josephdavidruiz@gmail.com>
2026-03-31 20:48:18 -0400

    Let's call it 0.8? yes

--- main.js
+++ main.js
@@ -324,7 +324,7 @@ document.addEventListener('DOMContentLoaded', async () => {
         const patch = {
             ...defaultPatches[defaultPatches.length - 1],
             workspaceTree: {
-                "version": "0.7.2",
+                "version": "0.8.0",
                 "activeWorkspaceId": 1,
                 "workspaces": [{"id": 1, "name": "Workspace 1"}]
             },
